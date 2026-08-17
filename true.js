b2bc9b4d990cf4d5f2da25c93e9dc35172e47c0c
tj-commits <thefoxjones@gmail.com>
2026-08-17 11:43:28 -0500

    This is why we don't have flying cars

--- true.js
+++ true.js
@@ -1,16 +1,5 @@
 // Critical enterprise code.
+const check = require("casting-out-nines")
+const nine = require("@positive-numbers/nine")
 
-module.exports = (() =>
-  arguments.constructor
-    .values(arguments.constructor())
-    .constructor.name.includes(
-      // eslint-disable-next-line 10x-engineering/no-tostring
-      arguments.constructor.prototype.toString
-        .call(
-          arguments.constructor.values(arguments.constructor()),
-        )
-        .split("[object ")
-        .pop()
-        // eslint-disable-next-line unicorn/prefer-number-properties, 10x-engineering/no-valueof
-        .split("]")[NaN.constructor.prototype.valueOf()],
-    ))()
+module.exports = check(nine,nine,nine)
